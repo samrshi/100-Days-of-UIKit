01f07ab31ef0a79115e10cf41df755280190dfcc
samrshi <samrshi@outlook.com>
2021-03-17 02:22:17 -0400

    Day 33

---
+++ Project 7/Project 7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 7
+//
+//  Created by Samuel Shi on 3/17/21.
+//
+
+import Foundation
+
+struct Petition: Codable {
+  var title: String
+  var body: String
+  var signatureCount: Int
+}
---
+++ Project 7/Project 7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by Samuel Shi on 3/17/21.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+  var results: [Petition]
+}

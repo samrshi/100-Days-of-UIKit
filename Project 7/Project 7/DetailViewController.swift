cc9ec1776d4b1cdb90043cfbc81110390f8fa084
samrshi <samrshi@outlook.com>
2021-03-19 02:43:30 -0400

    Day 35

--- Project 7/Project 7/DetailViewController.swift
+++ Project 7/Project 7/DetailViewController.swift
@@ -19,15 +19,33 @@ class DetailViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // challenge 3
+    var signatureText: String = ""
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .decimal
+    if let count = detailItem?.signatureCount {
+      if let formattedCount = formatter.string(from: NSNumber(value: count)) {
+        signatureText = "\(formattedCount) signature\(count == 1 ? "" : "s")"
+      }
+    }
+    
     guard let detailItem = detailItem else { return }
     let html = """
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style> body { font-size: 150%; } </style>
+        <style> body { font-size: 125%; } </style>
       </head>
       <body>
-        \(detailItem.body)
+        <h2>
+          \(detailItem.title)
+        </h2>
+        <h3>
+          \(signatureText)
+        </h3>
+        <p>
+          \(detailItem.body)
+        </p>
       </body>
     </html>
     """
--- Project 7/Project 7/ViewController.swift
+++ Project 7/Project 7/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UITableViewController {
   var petitions = [Petition]()
+  var filteredPetitions = [Petition]()
   
+  enum ButtonType {
+    case credits, filter, clearFilter
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    navigationItem.rightBarButtonItem = makeButton(buttonType: .credits)
+    navigationItem.leftBarButtonItem = makeButton(buttonType: .filter)
+    
     let urlString: String
     if navigationController?.tabBarItem.tag == 0 {
       urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -28,6 +37,42 @@ class ViewController: UITableViewController {
     showError()
   }
   
+  // challenge 2
+  @objc func search() {
+    let ac = UIAlertController(title: "Filter Results", message: nil, preferredStyle: .alert)
+    
+    ac.addTextField()
+    ac.addAction(UIAlertAction(title: "Filter", style: .default) { [weak self, weak ac] _ in
+      guard let filterKey = ac?.textFields?[0].text?.lowercased() else { return }
+      if filterKey.isEmpty { return }
+      self?.submitFilter(key: filterKey)
+    })
+    ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+    present(ac, animated: true)
+    navigationItem.leftBarButtonItem = makeButton(buttonType: .clearFilter)
+  }
+  
+  func submitFilter(key: String) {
+    filteredPetitions = petitions.filter { petition in
+      return petition.body.lowercased().contains(key) ||
+        petition.title.lowercased().contains(key)
+    }
+    tableView.reloadData()
+  }
+  
+  @objc func clearFilter() {
+    self.filteredPetitions = petitions
+    tableView.reloadData()
+    navigationItem.leftBarButtonItem = makeButton(buttonType: .filter)
+  }
+  
+  // challenge 1
+  @objc func showCredits() {
+    let ac = UIAlertController(title: "Credits", message: "This data comes fromthe We The Pepople API of the Whitehouse", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    present(ac, animated: true)
+  }
+  
   func showError() {
     let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
     ac.addAction(UIAlertAction(title: "OK", style: .default))
@@ -39,17 +84,31 @@ class ViewController: UITableViewController {
     
     if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
       petitions = jsonPetitions.results
+      filteredPetitions = petitions
       tableView.reloadData()
     }
   }
   
+  func makeButton(buttonType: ButtonType) -> UIBarButtonItem {
+    let button: UIBarButtonItem
+    switch buttonType {
+    case .credits:
+      button = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+    case .filter:
+      button = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(search))
+    case .clearFilter:
+      button = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(clearFilter))
+    }
+    return button
+  }
+  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return petitions.count
+    return filteredPetitions.count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-    let petition = petitions[indexPath.row]
+    let petition = filteredPetitions[indexPath.row]
     cell.textLabel?.text = petition.title
     cell.detailTextLabel?.text = petition.body
     return cell
@@ -57,7 +116,7 @@ class ViewController: UITableViewController {
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     let vc = DetailViewController()
-    vc.detailItem = petitions[indexPath.row]
+    vc.detailItem = filteredPetitions[indexPath.row]
     navigationController?.pushViewController(vc, animated: true)
   }
 }

a83ac51de86b0e2a8275ba084a69dae579b622e7
samrshi <samrshi@outlook.com>
2021-03-19 01:06:38 -0400

    Day 34

--- Project 7/Project 7/ViewController.swift
+++ Project 7/Project 7/ViewController.swift
@@ -12,19 +12,31 @@ class ViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    let urlString: String
+    if navigationController?.tabBarItem.tag == 0 {
+      urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+    } else {
+      urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+    }
     
-//    let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-    let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
     if let url = URL(string: urlString) {
       if let data = try? Data(contentsOf: url) {
-           parse(json: data)
+        parse(json: data)
+        return
       }
     }
+    showError()
+  }
+  
+  func showError() {
+    let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    present(ac, animated: true)
   }
   
   func parse(json: Data) {
     let decoder = JSONDecoder()
-  
+    
     if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
       petitions = jsonPetitions.results
       tableView.reloadData()
@@ -41,6 +53,12 @@ class ViewController: UITableViewController {
     cell.textLabel?.text = petition.title
     cell.detailTextLabel?.text = petition.body
     return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let vc = DetailViewController()
+    vc.detailItem = petitions[indexPath.row]
+    navigationController?.pushViewController(vc, animated: true)
   }
 }
 

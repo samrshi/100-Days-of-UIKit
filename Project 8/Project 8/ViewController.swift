8a0ce34ade15e41571314be3fc0c5e9d1167eaeb
samrshi <samrshi@outlook.com>
2021-03-20 04:20:41 -0400

    Day 36 Part 1

---
+++ Project 8/Project 8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 8
+//
+//  Created by Samuel Shi on 3/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

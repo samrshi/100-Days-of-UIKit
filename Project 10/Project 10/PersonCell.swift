51a8f7a63b3c0cae7b184d54dc74c84e0084d046
samrshi <samrshi@outlook.com>
2021-03-27 18:19:18 -0400

    Day 42

---
+++ Project 10/Project 10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Samuel Shi on 3/27/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var name: UILabel!
+  
+}
---
+++ Project 10/Project 10/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project 10
+//
+//  Created by Samuel Shi on 3/27/21.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+      fatalError("Unable to dequeue PersonCell.")
+    }
+    
+    return cell
+  }
+}
+

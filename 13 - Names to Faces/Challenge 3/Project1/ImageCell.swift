c2ccc494dd7513b1a92833ac787798905c132087
samrshi <samrshi@outlook.com>
2021-03-28 22:50:29 -0400

    Day 44

---
+++ 13 - Names to Faces/Challenge 3/Project1/ImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Project1
+//
+//  Created by Samuel Shi on 3/28/21.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+  @IBOutlet var name: UILabel!
+  @IBOutlet var image: UIImageView!
+}

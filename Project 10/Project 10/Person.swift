718566607488262c83526317dbf8a18e08173783
samrshi <samrshi@outlook.com>
2021-03-28 20:59:04 -0400

    Day 43

---
+++ Project 10/Project 10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Samuel Shi on 3/28/21.
+//
+
+import UIKit
+
+class Person: NSObject {
+  var name: String
+  var image: String
+  
+  init(name: String, image: String) {
+    self.name = name
+    self.image = image
+  }
+}
